c6d6e609e7e0d48d95a1717793efeca8a8600545
JhonyDark <jhonygestoru@gmail.com>
2023-05-07 17:20:20 -0500

    login terminado

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        btnprimary: "#4c7397",
+        btnsecondary: "#02DBBD",
+        secondary:{
+          900: "#131517",
+          100: "#1d2029",
+        }
+
+      }
+    },
   },
   plugins: [],
 }
