5e54ebac2193132c064cafcebed6d2b98562974a
JhonyDark <jhonydark@pop-os.localdomain>
2023-05-10 10:23:42 -0500

    continuacion diseño del home

--- src/pages/admin/Home.jsx
+++ src/pages/admin/Home.jsx
@@ -2,12 +2,19 @@ import React from 'react'
 // LINKS
 import { Link } from 'react-router-dom';
 // ICONOS
-import { RiHome3Line, RiStore2Line, RiShoppingCart2Line, RiCustomerService2Fill, RiLoginBoxLine, RiCheckboxBlankCircleFill, RiMenuUnfoldFill } from "react-icons/ri";
+import { RiHome3Line, RiStore2Line, RiShoppingCart2Line, RiCustomerService2Fill, RiLoginBoxLine, RiCheckboxBlankCircleFill, RiMenuUnfoldFill, RiCloseLine, RiNotification3Line, RiSearchEyeLine } from "react-icons/ri";
+import { useState } from 'react';
 
 const Home = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  }
+
   return (
     <div className='bg-secondary-900 min-h-screen'>
-       <sidebar className='fixed left-0 top-0 w-52 h-full overflow-y-scroll border-r border-btnsecondary p-8 flex flex-col justify-between'>
+       <sidebar className={`bg-secondary-900 fixed  top-0 w-52 h-full overflow-y-scroll border-r border-btnsecondary p-8 flex flex-col  justify-between transition-all duration-700 lg:left-0 z-50 ${showMenu ? "left-0" : "-left-full"}`}>
           <div>
             {/* Logo */}
             <h1 className='text-gray-300 uppercase text-2xl mb-10' >LOGO</h1>
@@ -36,7 +43,7 @@ const Home = () => {
             <ul>
               <li>
                 {/* este sin logear */}
-                <Link to="/" className='flex items-center gap-4 hover:bg-btnprimary hover:text-white py-4 px-2 rounded-xl transition-colors duration-500'>
+                <Link to="/auth" className='flex items-center gap-4 hover:bg-btnprimary hover:text-white py-4 px-0 2xl:px-2 rounded-xl transition-colors duration-500'>
                   <RiLoginBoxLine /> Iniciar Sesión
                 </Link>
                 {/* este cuando este logeado */}
@@ -54,10 +61,33 @@ const Home = () => {
           </ul>
           </div>
           {/* Btn menu movil */}
-          <button className='bg-btnprimary text-white fixed bottom-4 right-6 p-2 text-lg rounded-full'>
-            <RiMenuUnfoldFill />
+          <button onClick={toggleMenu} className='lg:hidden bg-btnprimary text-white fixed bottom-4 right-6 p-2 text-lg rounded-full z-50'>
+            {showMenu ? <RiCloseLine /> : <RiMenuUnfoldFill />}
           </button>
        </sidebar>
+
+       {/* Header */}
+       <header className='lg:pl-56 fixed w-full flex flex-col md:flex-row items-center justify-between gap-4 p-8' >
+          <nav className='flex items-center gap-4 order-1'>
+              <Link to="#" className='hover:bg-btnprimary text-white py-2 px-4 rounded-lg '>Fotografia</Link>
+              <Link to="#" className='hover:bg-btnprimary text-white py-2 px-4 rounded-lg '>Arte</Link>
+              <Link to="#" className='hover:bg-btnprimary text-white py-2 px-4 rounded-lg '>Escultura</Link>
+          </nav>
+          <ul className='flex items-center gap-4'>
+            <li>
+              <Link to="#" className='text-white text-lg'>
+                {" "}
+                  <RiNotification3Line />{" "}
+              </Link>
+            </li>
+            <li>
+              <form action="" className='relative'>
+                <RiSearchEyeLine className='text-white absolute top-3 left-2'/>
+                <input type="text" className='bg-secondary-100 outline-none border border-btnprimary text-white py-2 pl-8 pr-4 rounded-lg opacity-70 w-full' placeholder='Buscar' />
+              </form>
+            </li>
+          </ul> 
+       </header>
     </div>
   )
 }
