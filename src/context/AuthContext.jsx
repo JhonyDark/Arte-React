c89a9fa380e74999e1318fd8955a3f5a9ade3706
JhonyDark <jhonygestoru@gmail.com>
2023-06-15 20:06:42 -0500

    cambio de direccion de api

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,70 +1,64 @@
-import { createContext, useState } from 'react';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import { createContext, useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [errors, setErrors] = useState({});
 
-    const [user, setUser] = useState(null);
-    const [errors, setErrors] = useState({});
+  const navigation = useNavigate();
 
-    const navigation = useNavigate();
+  const login = async (userData) => {
+    try {
+      const response = await axios.post("http://art-api.test/api/login", {
+        email: userData.email,
+        password: userData.password,
+      });
 
+      setUser(response.data.user);
 
-    const login = async (userData) => {
+      localStorage.setItem("token", response.data.token);
 
-        try {
-            const response = await axios.post('http://back-art.test/api/login', {
-                email: userData.email,
-                password: userData.password
-            })
+      console.log(response);
 
-            setUser(response.data.user);
+      navigation("/home");
+    } catch (error) {
+      setErrors(error.response.data.errors);
+    }
+  };
 
-            localStorage.setItem('token', response.data.token);
+  const register = async (userData) => {
+    try {
+      const response = await axios.post("http://art-api.test/api/register", {
+        name: userData.name,
+        email: userData.email,
+        password: userData.password,
+        password_confirmation: userData.passwordConfirmation,
+      });
 
-            console.log(response);
+      console.log(response);
 
-            navigation('/home');
+      navigation("/login");
+    } catch (error) {
+      setErrors(error.response.data.errors);
+    }
+  };
 
-        } catch (error) {
-            setErrors(error.response.data.errors);
-        }
+  const logout = async () => {
+    // Aquí puedes realizar la lógica de cierre de sesión
+    // Por ejemplo, eliminar el token de autenticación y limpiar los datos del usuario del estado
 
-    };
+    setUser(null);
+    localStorage.removeItem("token");
+  };
 
-    const register = async (userData) => {
-        try {
-            const response = await axios.post('http://back-art.test/api/register', {
-                name: userData.name,
-                email: userData.email,
-                password: userData.password,
-                password_confirmation: userData.passwordConfirmation
-            })
-
-            console.log(response);
-
-            navigation('/login');
-
-        } catch (error) {
-            setErrors(error.response.data.errors);
-        }
-    };
-
-    const logout = async () => {
-        // Aquí puedes realizar la lógica de cierre de sesión
-        // Por ejemplo, eliminar el token de autenticación y limpiar los datos del usuario del estado
-
-        setUser(null);
-        localStorage.removeItem('token');
-    };
-
-    return (
-        <AuthContext.Provider value={{ user, errors, login, register, logout }}>
-            {children}
-        </AuthContext.Provider>
-    );
+  return (
+    <AuthContext.Provider value={{ user, errors, login, register, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export { AuthContext, AuthProvider };
